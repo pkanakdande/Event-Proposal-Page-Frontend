9a6195e03051b321d9832b44d99d6fa0f44dc640
mohitsahu21 <mohitsahu1993@gmail.com>
2023-04-22 20:47:25 +0530

    frontend latest changes

--- my-app/src/components/Vendor/Vendorhome.js
+++ my-app/src/components/Vendor/Vendorhome.js
@@ -1,10 +1,62 @@
 import React from "react";
+import  { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Vendorhome.css";
 import Home from "../Home/Home";
 import Userhome from "../User/Userhome";
+import { useNavigate } from "react-router-dom";
 
 function VendorHome() {
+
+  const navigate = useNavigate();
+  const [formData,setFormData]=useState({
+    email:"",
+    password:""
+  });
+ 
+  const {email,password} = formData
+  function updateData(e,propName){
+    let temp=e.target.value
+    setFormData(data =>({
+     ...data,[propName]:temp
+    }))
+    
+     }
+  // function updateData(e,[propName]){
+  //   let temp=e.target.value
+  //   setFormData(data => ({
+  //     ...data,[propName]: temp
+  //   }))
+  // }
+
+  async function submitted(e){
+    e.preventDefault()
+    // const data=new FormData(e.target)
+    console.log(formData)
+
+     
+     fetch("/login",{
+      method:"POST",
+      headers:{"content-type":"application/json","accept":"application/json"},
+      body:JSON.stringify(formData)
+      
+  })
+  .then((res)=>res.json())
+  .then((data)=>{
+     localStorage.setItem('token', data.data)
+     if (data.status == "ok")
+     {
+      alert("login Successful")
+      navigate("/VendorProposal")
+     }if (data.status == "error"){
+      alert(`${data.error}`)
+    }
+     
+    console.log(data)})
+  .catch((err)=>{
+    console.log(err)})
+   }
+
     return (<>
         <Home/>
         <div className="Logo">LOGO</div>
@@ -19,14 +71,14 @@ function VendorHome() {
             <h2>Sign in your Account</h2>
           </div>
           <div className="form">
-            <form method="post">
+            <form method="post"  onSubmit={submitted}>
               <div className="input">
                 {" "}
-                <input type="name" placeholder="Phone/Email" style={{width:'300px'}}/>{" "}
+                <input type="email" placeholder="Phone/Email" style={{width:'300px'}} name="email" onChange={e=>updateData(e,"email")} required/>{" "}
               </div>
               <div className="input">
                 {" "}
-                <input type="password" placeholder="Password" style={{width:'300px'}} />{" "}
+                <input type="password" placeholder="Password" style={{width:'300px'}} name="password"  onChange={e=>updateData(e,"password")} required />{" "}
               </div>
               <div className="forget">
                 {" "}
@@ -38,11 +90,11 @@ function VendorHome() {
                   {" "}
                   <Link to="/createvendoraccount" style={{textDecoration:'none'}}>Create Account</Link>{" "}
                 </div>
-                <Link to='/VendorProposal'>
+                {/* <Link to='/VendorProposal'> */}
                 <button className="button" type="submit">
                   SIGN IN
                 </button>
-                </Link>
+                {/* </Link> */}
               </div>
             </form>
           </div>
